8b99d7fee22fad0ecfab50bc3de8478ecfa8c851
Chase_Hartley <chase.hartley@my.jcu.edu.au>
2019-08-19 13:24:43 +1000

    refactor password program to use functions.

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -1,11 +1,25 @@
 
 MINIMUM_LENGTH = 5
-password = input("Please enter a password (The password must be longer than {} characters): ".format(MINIMUM_LENGTH))
 
-while len(password) < MINIMUM_LENGTH:
-    print("Password does not meet the minimum length requirment of {} characters".format(MINIMUM_LENGTH))
-    password = input("Please enter the password again: ")
 
-for i in range(len(password)):
-    print("*", end='')
+def main():
+    password = get_password()
 
+    print_asterisks(password)
+
+
+def print_asterisks(password):
+    for i in range(len(password)):
+        print("*", end='')
+
+
+def get_password():
+    password = input(
+        "Please enter a password (The password must be longer than {} characters): ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        print("Password does not meet the minimum length requirment of {} characters".format(MINIMUM_LENGTH))
+        password = input("Please enter the password again: ")
+    return password
+
+
+main()
